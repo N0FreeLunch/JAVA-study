797aae58be4df4c463315ba5f58dd011c5f4350a
N0FreeLunch <consilience90@gmail.com>
2024-02-01 02:00:30 +0900

    변수 네이밍 방식 설명

---
+++ src/variable/ex/Example1.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("without variable");
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+
+        System.out.println("with variable");
+        int four = 4;
+        int three = 3;
+        System.out.println(four + three);
+        System.out.println(four - three);
+        System.out.println(four * three);
+    }
+}
