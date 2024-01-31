e7a71609bedbd6ab3e4178241c4aa2e99510d1e5
N0FreeLunch <consilience90@gmail.com>
2024-02-01 01:47:50 +0900

    변수의 타입과 범위

--- src/variable/Var7.java
+++ src/variable/Var7.java
@@ -1,2 +1,19 @@
-package variable;public class Var7 {
+package variable;
+
+import java.sql.SQLOutput;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100;
+        double b = 0.5;
+        boolean c = true;
+        char d = 'A';
+        String e = "Hello Java";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
 }
--- src/variable/Var8.java
+++ src/variable/Var8.java
@@ -1,2 +1,14 @@
-package variable;public class Var8 {
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        byte b = 127;
+//        byte bb = 32767;
+        short s = 32767;
+//        short ss = 2147483647;
+        int i = 2147483647;
+        long l = 9223372036854775807L;
+        float f = 10.8f;
+        double d = 10.0;
+    }
 }
