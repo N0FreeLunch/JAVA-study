a5761464ef13bcaaea1ed5901f5b0f754320f833
N0FreeLunch <consilience90@gmail.com>
2024-02-01 01:46:02 +0900

    초기화 하지 않을 경우 에러 케이스

--- src/variable/Var6.java
+++ src/variable/Var6.java
@@ -3,6 +3,9 @@ package variable;
 public class Var6 {
     public static void main(String[] args) {
         int a;
-        System.out.println(a);
+//        /*
+//         * 주석을 지우면 에러가 발생
+//         */
+//        System.out.println(a);
     }
 }
