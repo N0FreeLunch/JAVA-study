e04058a55904960900c7bbd53948cc553af36f3a
N0FreeLunch <consilience90@gmail.com>
2024-02-01 02:14:43 +0900

    예제 코드 추가

---
+++ src/variable/ex/Example3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class Example3 {
+    public static void main(String[] args) {
+        long bigNumber = 10000000000L;
+        System.out.println(bigNumber);
+
+        boolean flag = true;
+        System.out.println(flag);
+    }
+}
